d8c092b13577af5b5be3840250091eb2d12ed5ba
Vasil Khabibullin <gitkhab@yandex.ru>
2021-09-27 03:42:18 +0300

    Add table page meta

--- pages/table.vue
+++ pages/table.vue
@@ -1,3 +1,18 @@
 <template>
   <div>*table page*</div>
 </template>
+
+<script>
+  export default {
+    head: {
+      title: 'Экран №1',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Стильная столешница с меламиновым покрытием и усиленной стальной рамой.'
+        }
+      ]
+    }
+  }
+</script>
