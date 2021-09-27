9ec8b4a756a591bafb0a33325a9c73c96a4b232a
Vasil Khabibullin <gitkhab@yandex.ru>
2021-09-27 12:51:07 +0300

    Add voice-control meta

--- pages/voice-control/index.vue
+++ pages/voice-control/index.vue
@@ -1,3 +1,18 @@
 <template>
   <div>*voice control page*</div>
 </template>
+
+<script>
+  export default {
+    head: {
+      title: 'Экран №2',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '«ОКЕЙ, ЯМАГУЧИ». Теперь вы можете управлять программами массажа или самим креслом с помощью голосовых команд!'
+        }
+      ]
+    }
+  }
+</script>
