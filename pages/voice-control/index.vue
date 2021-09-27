a69bd35978764229a0752b6ca7113339c248c3da
Vasil Khabibullin <gitkhab@yandex.ru>
2021-09-27 13:35:30 +0300

    Add basic content

--- pages/voice-control/index.vue
+++ pages/voice-control/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>*voice control page*</div>
+  <div class="voice">
+    <div class="voice__inner">
+      <h1 class="voice__title">Yamaguchi Voice Control</h1>
+      <p class="voice__paragraph voice__paragraph_1">
+        Вы&nbsp;можете управлять программами массажа или&nbsp;самим креслом с&nbsp;помощью голосовых команд, приведённых
+        ниже. Коснитесь кратковременно кнопки голосового управления, расположенной на&nbsp;левом подлокотнике,
+        для&nbsp;запуска сеанса голосового управления.
+      </p>
+      <p class="voice__paragraph voice__paragraph_2">
+        Произнесите одну из&nbsp;доступных команд. Если&nbsp;произнесённая команда распознана, вы&nbsp;услышите ответ
+        и&nbsp;кресло приступит к&nbsp;выполнению запроса.
+      </p>
+      <!-- TODO: Заменить заглушку на ассистента -->
+      <div class="voice__assistant">*assistant*</div>
+      <!-- TODO: Добавить путь к изображению -->
+      <img class="voice__image" src="#" alt="Чат с голосовым помощником Yamaguchi">
+      <!-- TODO: Заменить заглушку на голосовую команду -->
+      <div class="voice__command">*assistant*</div>
+    </div>
+  </div>
 </template>
 
 <script>
