f7541f2062f8c081b897746ded7039bec2e87b43
Vasil Khabibullin <gitkhab@yandex.ru>
2021-09-27 23:13:05 +0300

    Add assistant animation

--- components/TheVoiceAssistant.vue
+++ components/TheVoiceAssistant.vue
@@ -9,7 +9,35 @@
 
 <script>
   export default {
-    name: 'TheVoiceAssistant'
+    name: 'TheVoiceAssistant',
+    mounted() {
+      this.callAssistant()
+    },
+    methods: {
+      callAssistant() {
+        const timeline = this.$gsap.timeline()
+        timeline.to('.voice-assistant__ellipse', {
+          transform: 'scale(1.5)',
+          ease: 'Back.easeOut'
+        })
+        timeline.to('.voice-assistant__image', {
+          transform: 'translateY(-40px)',
+          ease: 'Power3.easeInOut',
+          duration: 1
+        })
+        timeline.to('.voice-assistant__image', {
+          transform: 'translateY(0)',
+          ease: 'Power1.easeInOut',
+          duration: 1.75,
+          repeat: -1,
+          yoyo: true
+        })
+        timeline.to('.voice-assistant__ellipse', {
+          transform: 'scale(1)',
+          ease: 'Back.easeOut'
+        }, '<')
+      }
+    }
   }
 </script>
 
@@ -20,21 +48,27 @@
   }
 
   .voice-assistant__image-wrapper {
-    margin-bottom: -30px;
     overflow: hidden;
+    position: relative;
+    z-index: 550;
+    padding-top: 40px;
+    margin-bottom: -30px;
   }
 
   .voice-assistant__image {
     position: relative;
-    bottom: -100px;
     width: 100%;
+    transform: translateY(800px);
   }
 
   .voice-assistant__ellipse {
+    position: relative;
+    z-index: 500;
     width: 194px;
     height: 61px;
     margin: 0 auto;
     border-radius: 50%;
     background-color: #000;
+    transform: scale(0);
   }
 </style>
