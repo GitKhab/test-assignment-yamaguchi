6d9f0773675c922bdd72cf9d18baf7ce5751bd43
Vasil Khabibullin <gitkhab@yandex.ru>
2021-09-27 03:33:34 +0300

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <Nuxt class="layout" />
+</template>
+
+<style lang="scss">
+  html {
+    height: 100%;
+  }
+
+  body {
+    height: inherit;
+    min-width: 320px;
+    margin: 0;
+  }
+
+  #__nuxt {
+    height: inherit;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #__layout {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .layout {
+    flex-grow: 1;
+  }
+</style>
